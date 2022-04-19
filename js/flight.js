77a6979947846d97030a0ed47031e866beb6f236
Anton <kondratov247@mail.ru>
2022-04-19 11:46:55 +0700

    Final

--- js/flight.js
+++ js/flight.js
@@ -1,8 +1,52 @@
+let start = 0;
+let stopAnim = 0;
+let timerFlight;
 
-function animation() {
-    document.getElementById('flight_header').style.ЦС
+let StopCheck = setInterval(() => {
+  if(stopAnim == 1){
+    stopAnim = 0;
+    timerFlight = setInterval(() => {
+      animation()
+      }, 1200000)
   }
+
+  },10 )
+
 
 
 // повторить с интервалом 2 секунды
-let timerIdFlight = setInterval(animation, 10000);
+function animation(){
+    clearInterval(timerFlight);
+    let scene = document.getElementsByClassName('bg_plane')
+    scene = scene[0]
+    scene.style.opacity = "1";
+    let op = 1;
+    scene.style.opacity = op;
+
+    let Time = setTimeout(() => {
+      start = 1;
+    }, 4000);
+
+    let TimeCheck = setInterval(() => {
+      if(start == 1){
+        clearInterval(TimeCheck);
+        
+        let interval = setInterval(() => {  
+          if(op == 0 || op < 0){
+            start = 0;
+            stopAnim = 1;
+            clearInterval(interval);
+          }
+          console.log(op) 
+          op -= 0.005
+          scene.style.opacity = op;
+
+        }, 20);    
+
+      }
+    }, 10)
+
+}
+
+animation()
+
---
+++ pen-export-xEXPRN/README.md
@@ -0,0 +1,5 @@
+# CodePlane
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/sreisner/pen/xEXPRN](https://codepen.io/sreisner/pen/xEXPRN).
+
+It's CodePlane!  Brought to you by CSS 3D transforms and SVG!
